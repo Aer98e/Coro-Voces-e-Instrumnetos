1fab1bced704e2dd92daa7fe30576d69f63cd339
Aarón Pérez <aaronestebanperezespinal@gmail.com>
2026-06-07 08:35:59 -0500

    Update vite.config.js
    Cambie la base a '/'

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   // GitHub Pages URL: https://aer98e.github.io/Coro-Voces-e-Instrumnetos/
-  base: '/Coro-Voces-e-Instrumnetos/',
+  base: '/',
   root: '.',
   build: {
     outDir: 'dist',
